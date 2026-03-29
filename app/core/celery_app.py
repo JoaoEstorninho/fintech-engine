3bdf1b9e6f27c951bc4b502ff2f2b81288b4317f
JoaoEstorninho <joao.estorninho5@gmail.com>
2026-03-29 20:30:58 +0100

    Fix Celery task discovery2

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -6,7 +6,6 @@ REDIS_URL = os.getenv("REDIS_URL")
 celery_app = Celery(
     "worker",
     broker=REDIS_URL,
-    backend=REDIS_URL
+    backend=REDIS_URL,
+    include=["app.tasks.payment_tasks"]
 )
-
-celery_app.autodiscover_tasks(["app.tasks"])
