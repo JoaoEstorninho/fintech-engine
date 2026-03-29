2974fe5941ca4d703df1474d3db063d5fe6c851f
JoaoEstorninho <joao.estorninho5@gmail.com>
2026-03-29 23:16:26 +0100

    fix(celery): db config

--- app/core/db.py
+++ app/core/db.py
@@ -4,7 +4,12 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"}  # 🔥 THIS LINE
+)
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from app.api.routes import router
 from app.core.logging_config import setup_logging
 from app.core.db import Base, engine
+from app.core.exceptions import AppException, app_exception_handler
 
+app.add_exception_handler(AppException, app_exception_handler)
 Base.metadata.create_all(bind=engine)
 
 setup_logging()
