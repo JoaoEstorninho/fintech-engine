a93588fb425d0fed3aa4157eb7289a2e50e603a7
JoaoEstorninho <joao.estorninho5@gmail.com>
2026-03-30 23:00:15 +0100

    debug(celery): add logging and proper task execution handling

--- app/main.py
+++ app/main.py
@@ -4,20 +4,15 @@ from app.core.logging_config import setup_logging
 from app.core.db import Base, engine
 from app.core.exceptions import AppException, app_exception_handler
 
-# Setup logging first
 setup_logging()
 
-# Create app
 app = FastAPI()
 
-# Register exception handler
 app.add_exception_handler(AppException, app_exception_handler)
 
-# Register routes
 app.include_router(router)
 
 
-# ✅ Better: run DB init on startup
 @app.on_event("startup")
 def on_startup():
     Base.metadata.create_all(bind=engine)
--- app/tasks/payment_tasks.py
+++ app/tasks/payment_tasks.py
@@ -1,9 +1,20 @@
 from app.core.celery_app import celery_app
 from app.services.use_cases.process_payment import ProcessPaymentService
+import logging
 
-service = ProcessPaymentService()
+logger = logging.getLogger(__name__)
 
 
 @celery_app.task()
 def process_payment_task(payment_id: str):
-    service.execute(payment_id)
+    logger.info(f"TASK RECEIVED: {payment_id}")
+
+    try:
+        service = ProcessPaymentService()
+        service.execute(payment_id)
+
+        logger.info(f"TASK COMPLETED: {payment_id}")
+
+    except Exception as e:
+        logger.error(f"TASK FAILED: {payment_id} - {str(e)}", exc_info=True)
+        raise
